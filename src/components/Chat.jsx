8ad8ab6ac5c07197aeab91bcf1051d80fab7e8be
Tatsuya <yokokura0413@gmail.com>
2021-01-11 05:58:52 +0900

    feat: cssの切り分け

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 
 const Chat = ({ text, type }) => {
   const isQuestion = (type === 'question');
+  const classes = isQuestion ?
+                  'p-chat__row' :
+                  'p-chat__reverse';
 
   return (
-    <ListItem>
+    <ListItem className={classes}>
       { console.log(isQuestion) }
       <ListItemAvatar>
         <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
